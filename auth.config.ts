349c69e525c1cfbf712a9204d81bff9dcfc56cb1
Nathan Will Martins <nathanwillmartins@gmail.com>
2026-07-09 21:31:52 -0300

    fix: Correção no login via google

--- auth.config.ts
+++ auth.config.ts
@@ -2,7 +2,12 @@ import type { NextAuthConfig } from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const authConfig: NextAuthConfig = {
-  providers: [Google],
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
   pages: { signIn: "/login" },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
